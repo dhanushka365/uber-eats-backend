b8cc515870c2f163988a69755ad37d6e17294fd9
uduwela96 <puduwel@mitrai.com>
2022-12-23 12:43:23 +0530

    user accounts not created need to resolve

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -15,5 +15,27 @@ export class UsersResolver{
  }
 
  @Mutation(returns => CreateAccountOutput)
- createAccount(@Args("Input") createAccountInput:CreateAccountInput){}
+ async createAccount(@Args('input') createAccountInput:CreateAccountInput
+ ): Promise<CreateAccountOutput>{
+   try {
+      const error = await this.userService.createAccount(createAccountInput);
+      if (error) {
+         return{
+            ok: false,
+            error,
+            
+         };
+      }
+      return{
+         ok:true,
+      };
+   } catch (e) {
+      return {
+         error:e,
+         ok:false,
+         
+      }
+   }
+
+ }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,9 +2,29 @@ import { User } from "./entities/user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import {Repository} from 'typeorm'
 import { Injectable } from "@nestjs/common";
+import { CreateAccountInput } from "./dtos/create-account.dto";
 
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private readonly users: Repository<User>){} 
+    constructor(@InjectRepository(User) private readonly users: Repository<User>,
+    ){} 
+
+
+    async createAccount({
+        email, 
+        password,
+        role,
+    }:CreateAccountInput): Promise<string | undefined>{
+        try {
+            const exists = await this.users.findOne({});
+            if (exists) {
+                return 'Thereis a user with that email already';
+            }
+
+            await this.users.save(this.users.create({email , password, role}));
+        }catch (e) {
+         return "Couldn't create account";
+     }
+    }
 }
